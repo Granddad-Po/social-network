cbcd1e96c8da66e95446934b80039d401c4fc2f3
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2021-11-16 10:34:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<ul>
+				<li>News</li>
+				<li>Photos</li>
+				<li>Contacts</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
