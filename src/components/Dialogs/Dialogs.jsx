59b4d4ca57a82e83e667c12c7bcc071a9272ae29
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2021-11-22 17:15:20 +0300

    refactor: edit message page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,36 @@
 import s from './Dialogs.module.css';
 
 const Dialogs = () => {
-    return (
-        <div>
-            Dialogs
-        </div>
-    )
+	return (
+		<div className={s.dialogs}>
+			<div className={s.dialogsItem}>
+				<div className={s.dialog + ' ' + s.active}>
+					Vitaliy
+				</div>
+				<div className={s.dialog}>
+					Tima
+				</div>
+				<div className={s.dialog}>
+					Vlad
+				</div>
+				<div className={s.dialog}>
+					Lana
+				</div>
+				<div className={s.dialog}>
+					Arthur
+				</div>
+				<div className={s.dialog}>
+					Daria
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hello</div>
+				<div className={s.message}>How are you doing?</div>
+				<div className={s.message}>Hello. Im fine. Thanks.</div>
+				<div className={s.message}>And you?</div>
+			</div>
+		</div>
+	)
 }
 
 export default Dialogs;
