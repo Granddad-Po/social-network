a48cb79e3939958407fc117a6780de79ee06a6c0
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2022-01-03 11:10:01 +0300

    add: post, delete requests on social-network server and create follow/unfollow functional

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import s from "./Users.module.css";
 import userPhoto from "../../images/user.png";
 import React from "react";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
 
@@ -35,8 +36,31 @@ let Users = (props) => {
                         </div>
                         <div>
                     {u.followed ?
-                        <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                        : <button onClick={() => props.follow(u.id)}>Follow</button>
+                        <button onClick={ () => {
+                            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                            withCredentials: true,
+                                headers: {
+                                    'API-KEY': 'ea7e661d-f340-4c0e-b12e-b599197c412e'
+                                }
+                            }).then(response => {
+                            if (response.data.resultCode == 0) {
+                                props.unfollow(u.id)
+                            }
+                        })
+                        }}>Unfollow</button>
+                        : <button onClick={ () => {
+                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                withCredentials: true,
+                                headers: {
+                                    'API-KEY': 'ea7e661d-f340-4c0e-b12e-b599197c412e'
+                                }
+                            }).then(response => {
+                                if (response.data.resultCode == 0) {
+                                    props.follow(u.id)
+                                }
+                            })
+                        }
+                        }>Follow</button>
                     }
                         </div>
                     </span>
